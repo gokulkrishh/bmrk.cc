143cf997f348a9224f5c900fb257ead3eb017197
Gokulakrishnan Kalaikovan <2944237+gokulkrishh@users.noreply.github.com>
2024-01-24 19:26:36 +0530

    fix typescript error

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,18 +1,21 @@
 import { getBookmarksWithFilter } from 'app/actions/bookmarks';
-import { BookmarkModified } from 'types/data';
+import { getTags } from 'app/actions/tags';
 
 import Card from 'components/card';
 import Header from 'components/header';
 
+import { BookmarkModified } from 'types/data';
+
 export default async function Page() {
   const bookmarks = await getBookmarksWithFilter({ is_fav: true });
+  const tags = await getTags();
 
   return (
     <>
       <Header headerText="Favorites" />
       <div className="min-h-dvh border-r border-neutral-200">
         {bookmarks.map((bookmark: BookmarkModified) => (
-          <Card key={bookmark.id} data={bookmark} />
+          <Card tags={tags} key={bookmark.id} data={bookmark} />
         ))}
       </div>
     </>
--- lib/data.ts
+++ lib/data.ts
@@ -1,11 +1,11 @@
 import { Tag } from 'types/data';
 
-export const groupBy = (tags: Tag[], key: Tag['id']) => {
-  return tags.reduce(
-    (acc, tag) => {
-      acc[tag[key] as keyof Tag] = tag;
-      return acc;
-    },
-    {} as Record<string, Tag>
-  );
-};
+// export const groupBy = (tags: Tag[], key: Tag['id']) => {
+//   return tags.reduce(
+//     (acc, tag) => {
+//       acc[tag[key] as any] = tag;
+//       return acc;
+//     },
+//     {} as Record<string, Tag>
+//   );
+// };
