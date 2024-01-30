406a084d9d708bf767e602a233f093b5ccf6d5ba
Gokulakrishnan Kalaikovan <2944237+gokulkrishh@users.noreply.github.com>
2024-01-30 10:37:08 +0530

    fix auth callback and redirect

--- app/app/auth/callback/route.ts
+++ app/app/auth/callback/route.ts
@@ -14,7 +14,7 @@ export async function GET(request: Request) {
     const supabase = await createSupabaseServerClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      return NextResponse.redirect(`${origin}/app${next}`);
+      return NextResponse.redirect(`${origin}/${next}`);
     }
   }
   return NextResponse.redirect(urls.account);
