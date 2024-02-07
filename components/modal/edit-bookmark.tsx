ef4e2dcabbcb779cef2fbee0cab7911c9720c520
Gokulakrishnan Kalaikovan <2944237+gokulkrishh@users.noreply.github.com>
2024-02-07 22:43:08 +0530

    upload btn size adjust

--- components/modal/edit-bookmark.tsx
+++ components/modal/edit-bookmark.tsx
@@ -104,7 +104,7 @@ export default function EditBookmark({
           type="url"
           inputMode="url"
           pattern="https://.*|http://.*"
-          placeholder="https://google.com"
+          placeholder="https://x.com"
           required
           onChange={(event) => {
             setState((prev) => ({ ...prev, url: event.target.value }));
@@ -137,7 +137,7 @@ export default function EditBookmark({
             loading || !isUrl(state.url) || !state.title?.length || !isEdited()
           }
           className={cn(
-            `rounded-full transition-colors font-medium items-center bg-blue-600 hover:bg-blue-700 focus:bg-blue-700 active:bg-blue-700 disabled:opacity-40 disabled:active:bg-blue-600 disabled:hover:bg-blue-600 disabled:focus:bg-blue-600 border-0 flex justify-center py-2 px-5 text-white`,
+            `rounded-full h-[40px] transition-colors font-medium items-center bg-blue-600 hover:bg-blue-700 focus:bg-blue-700 active:bg-blue-700 disabled:opacity-40 disabled:active:bg-blue-600 disabled:hover:bg-blue-600 disabled:focus:bg-blue-600 border-0 flex justify-center py-2 px-4 text-white`,
             {
               '!opacity-50 cursor-not-allowed': loading,
             },
