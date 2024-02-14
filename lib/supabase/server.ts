00a9fdb1f08f2113c31a033632d72014ee1cbac2
Gokulakrishnan Kalaikovan <2944237+gokulkrishh@users.noreply.github.com>
2024-02-14 09:55:21 +0530

    infer database to supabase client

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -3,11 +3,12 @@ import { cookies } from 'next/headers';
 import { createServerClient } from '@supabase/ssr';
 
 import { createFetch } from './cache';
+import { Database } from 'types/supabase';
 
 export default function createClient(cacheTags: string[] = []) {
   const cookieStore = cookies();
 
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
