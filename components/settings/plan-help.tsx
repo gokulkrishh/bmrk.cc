fb0dece5210b488ee89d4dd519a284009a810ebb
Gokulakrishnan Kalaikovan <2944237+gokulkrishh@users.noreply.github.com>
2024-02-16 16:07:12 +0530

    Add support for click for tooltip in plans section

--- components/settings/plan-help.tsx
+++ components/settings/plan-help.tsx
@@ -1,14 +1,25 @@
 'use client';
 
+import { useState } from 'react';
+
 import { QuestionMarkCircledIcon } from '@radix-ui/react-icons';
 import { TooltipTrigger } from '@radix-ui/react-tooltip';
 
 import { Tooltip, TooltipContent } from 'components/ui/tooltip';
 
 export default function PlanTooltip({ text }: { text: string }) {
+  const [open, setOpen] = useState(false);
   return (
-    <Tooltip>
-      <TooltipTrigger asChild>
+    <Tooltip open={open} onOpenChange={setOpen}>
+      <TooltipTrigger
+        onClick={() => {
+          setOpen(true);
+        }}
+        onBlur={() => {
+          setOpen(false);
+        }}
+        asChild
+      >
         <QuestionMarkCircledIcon className="w-4 h-4 ml-1.5 cursor-pointer text-muted-foreground" />
       </TooltipTrigger>
       <TooltipContent side="top" className="text-white dark:text-black">
