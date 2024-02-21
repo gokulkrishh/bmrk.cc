13accac593e126b9384db3b31ddb5b30717261bd
Gokulakrishnan Kalaikovan <2944237+gokulkrishh@users.noreply.github.com>
2024-02-21 14:09:37 +0530

    fix wording for usage limit

--- config/index.ts
+++ config/index.ts
@@ -61,8 +61,8 @@ export const messages = {
   bookmarkLimit: (plan: string = plans.free.name) =>
     `Bookmarks in the import exceeds the allowed usage limit.`,
   tagLimit: (plan: string = plans.free.name) =>
-    `Tags count exceeds the allowed monhtly usage limit.`,
-  usageLimitRenewal: `Usage limit auto-renews on basis`,
+    `Tags count exceeds the allowed monthly usage limit.`,
+  usageLimitRenewal: `Usage limit auto-renews on monthly basis`,
   maxFileSize: `Maximum file size is 500 KB`,
   importLimitWarning: (count: number = 1) =>
     `Importing ${count} time${count > 1 ? 's' : ''} – doesn't count towards your monthly usage.`,
