a7192f1e5cc45ef27fd045215986658eeed52076
Gokulakrishnan Kalaikovan <2944237+gokulkrishh@users.noreply.github.com>
2024-02-28 13:02:17 +0530

    move search cmd dialog to top

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -30,7 +30,7 @@ interface CommandDialogProps extends DialogProps {}
 const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   return (
     <Dialog {...props}>
-      <DialogContent className="overflow-hidden p-0 shadow-lg max-sm:max-w-[calc(100%-16px)]">
+      <DialogContent className="overflow-hidden p-0 top-[30%] shadow-lg max-sm:max-w-[calc(100%-16px)]">
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
         </Command>
