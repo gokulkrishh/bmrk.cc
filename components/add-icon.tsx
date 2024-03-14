3255087a1f4a0a3942387fc39e49363d4dc671c1
Gokulakrishnan Kalaikovan <2944237+gokulkrishh@users.noreply.github.com>
2024-03-14 13:12:18 +0530

    remove font-medium for add icon

--- components/add-icon.tsx
+++ components/add-icon.tsx
@@ -38,7 +38,7 @@ export default function AddIcon({ className }: { className?: string }) {
         </TooltipTrigger>
         <TooltipContent
           side="right"
-          className="flex items-center ml-4 font-medium text-white dark:text-black"
+          className="flex items-center ml-4 text-white dark:text-black"
         >
           Add{' '}
           <kbd className="pointer-events-none ml-2 border dark:text-black text-white border-input inline-flex h-4 select-none items-center gap-1 rounded  px-1 font-mono text-[10px] font-medium text-primary opacity-100">
